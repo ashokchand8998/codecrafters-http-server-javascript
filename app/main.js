20293149e93ea27d92db49544e690ab3a81ee892
root <root@ACT-device>
2025-04-13 18:13:43 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,8 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on("data", () => {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    socket.on("data", (data) => {
+        const request = data.toString().split("\n");
+        const requestLine = request[0].split(' ')
+        const target = requestLine[1]
+        if (target === '/') {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+        }
         socket.emit("close");
     })
     socket.on("close", () => {
