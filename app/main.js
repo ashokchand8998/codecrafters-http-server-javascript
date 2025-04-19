ad1278cb3aad9516ab778c0c42cc7a110e17b986
root <root@ACT-device>
2025-04-19 20:11:32 +0530

    c

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const net = require("net");
-const fs = require("fs")
+const fs = require("fs");
+const zlib = require("zlib");
 
 const supportedEncodings = new Set(['gzip'])
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,7 +15,7 @@ const generateResponse = (val, contentType = 'text/plain', statuMsg = '200 OK') 
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on("data", (data) => {
+    socket.on("data", async (data) => {
         const request = data.toString().split("\r\n\r\n");
         const [requestLine, ...headers] = request[0].split('\r\n')
         const requestBody = request[1]
@@ -92,14 +93,24 @@ const server = net.createServer((socket) => {
             if (availableEncoding) {
                 // preform encoding
                 response.header += `\r\nContent-Encoding: ${availableEncoding}`
+                switch (availableEncoding) {
+                    case 'gzip': {
+                        const compressedBody = zlib.gzipSync(response.body);
+                        response.body = compressedBody
+                        response.header += `\r\nContent-Length: ${response.body.length}`
+                        break;
+                    }
+                }
             }
         }
         // socket.emit("close");
         if (closeConnection) {
-            socket.write(response.header + "\r\nConnection: close" + "\r\n\r\n" + response.body)
+            socket.write(response.header + "\r\nConnection: close" + "\r\n\r\n");
+            socket.write(response.body)
             socket.emit("close");
         } else {
-            socket.write(response.header + "\r\n\r\n" + response.body)
+            socket.write(response.header + "\r\n\r\n");
+            socket.write(response.body)
         }
     })
     socket.on("close", () => {
